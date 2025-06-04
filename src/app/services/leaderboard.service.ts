e66721b2ecfe231afc2eae70dddb025feccf3c3d
sparr0w24 <shaminsmashalkar@gmail.com>
2025-06-04 16:37:21 -0700

    moving db.json + switching api variable

--- src/app/services/leaderboard.service.ts
+++ src/app/services/leaderboard.service.ts
@@ -2,25 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from '../player.model'
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderboardService {
-  private apiUrl = 'http://localhost:3000/players';
+  //private apiUrl = 'http://localhost:3000/players';
 
   constructor(private http: HttpClient) { }
 
   getLeaderboard(sortField: string = 'score', sortOrder: string = 'desc'): Observable<Player[]> {
-    return this.http.get<Player[]>(`${this.apiUrl}?_sort=${sortField}&_order=${sortOrder}`);
+    return this.http.get<Player[]>(`${environment.apiUrl}?_sort=${sortField}&_order=${sortOrder}`);
   }
 
   addPlayer(player: Player): Observable<Player> {
-    return this.http.post<Player>(this.apiUrl, player);
+    return this.http.post<Player>(environment.apiUrl, player);
   }
 
   deletePlayer(id: number): Observable<Player> {
-    return this.http.delete<Player>(`${this.apiUrl}/${id}`);
+    return this.http.delete<Player>(`${environment.apiUrl}/${id}`);
   }
 
   //updatePlayer(player: Player): Observable<Player> {
