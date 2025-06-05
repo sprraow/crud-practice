992dbbc30123ca37302be426556ab132c1e9d8aa
sparr0w24 <shaminsmashalkar@gmail.com>
2025-06-04 17:27:30 -0700

    updating url

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'known-border-production.up.railway.app/players'
+}; 
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: process.env['API_URL'] || 'http://localhost:3000/players'
+  apiUrl: 'http://localhost:3000/players'
 };
